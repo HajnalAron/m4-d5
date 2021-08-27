19364639d1c3de622fefbe1c66858fedad2dd559
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-08-27 18:25:33 +0500

    footer has been implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyFooter from './Components/MyFooter';
 
 function App() {
   return (
     <div className="App">
+      <MyFooter />
     </div>
   );
 }
